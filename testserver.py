1db3999149fbe0446560762a4669e69f9cb83359
nocarryr <matt@nomadic-recording.com>
2023-05-23 15:38:14 -0500

    WIP - build weather widget with openweathermap

--- testserver.py
+++ testserver.py
@@ -1,9 +1,15 @@
+import os
 import asyncio
 from pathlib import Path
 import aiohttp
 from aiohttp import web
+from yarl import URL
+import jinja2
+import aiohttp_jinja2
+from dotenv import load_dotenv
 
 HERE = Path(__file__).resolve().parent
+API_KEY = os.environ['OPENWEATHERMAP_APIKEY']
 
 routes = web.RouteTableDef()
 
@@ -26,8 +32,39 @@ async def rss_calendar(request):
     resp_text = await get_rss_feed(url)
     return web.Response(text=resp_text, content_type='text/xml')
 
+async def get_geo_coords() -> tuple[float, float]:
+    query = {
+        'zip':'76063,US',
+        'appid':API_KEY,
+    }
+    url = URL('http://api.openweathermap.org/geo/1.0/zip').with_query(**query)
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            data = await response.json()
+            return data['lat'], data['lon']
+
+
+@routes.get('/weather2')
+@aiohttp_jinja2.template('weather2/openweather.html')
+async def get_weather_data(request):
+    lat, lon = await get_geo_coords()
+    query = {
+        'lat':lat,
+        'lon':lon,
+        'units':'imperial',
+        'lang':'en',
+        'appid':API_KEY,
+    }
+    url = URL('https://api.openweathermap.org/data/2.5/weather').with_query(**query)
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            data = await response.json()
+            return {'weather_data':data}
+
+
 def init_func(argv):
     app = web.Application()
+    aiohttp_jinja2.setup(app, loader=jinja2.FileSystemLoader(HERE))
     routes.static('/static', HERE)
     app.add_routes(routes)
     return app
