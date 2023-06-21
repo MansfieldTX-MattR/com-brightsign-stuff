760585b3b816e6b02aa0f070a56daf7f321856f1
nocarryr <matt@nomadic-recording.com>
2023-06-21 14:07:42 -0500

    Add debug display for last server update

--- weather2/openweather.js
+++ weather2/openweather.js
@@ -37,6 +37,7 @@ async function updateWeather(){
     const currentDiv = document.querySelector("main > .current");
     const newElems = weatherDoc.querySelectorAll("body > *");
     currentDiv.replaceChildren(...newElems);
+    showDt(currentDiv);
 }
 
 async function updateForecast(){
@@ -59,7 +60,32 @@ async function updateForecast(){
     const currentDiv = document.querySelector("main > .forecast");
     const newElems = weatherDoc.querySelectorAll("body > *");
     currentDiv.replaceChildren(...newElems);
+    showDt(currentDiv);
 }
+
+function showDt(elem){
+    const dtFmt = new Intl.DateTimeFormat(
+        'en-US',
+        {
+            month:'numeric',
+            day:'numeric',
+            hour:'2-digit',
+            minute:'numeric',
+        }
+    );
+    const p = elem.querySelector(".lastUpdate");
+    const timeStamp = parseInt(p.dataset.timeStamp) * 1000;
+    const dt = new Date(timeStamp);
+    const td = document.querySelector(p.dataset.target);
+    td.textContent = dtFmt.format(dt);
+    p.parentElement.removeChild(p);
+}
+
+document.addEventListener("DOMContentLoaded", (e) => {
+    document.querySelectorAll(".current, .forecast").forEach((elem) => {
+        showDt(elem);
+    });
+});
 
 function fetchAndUpdate(){
     if (window.stopWeather){
